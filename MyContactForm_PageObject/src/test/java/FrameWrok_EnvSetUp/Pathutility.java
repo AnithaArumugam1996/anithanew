620091ebd96f54503b25701b9e1738ac13f0fc41
Hp <Hp@DESKTOP-L64HF8G.lan>
2024-07-14 15:41:49 +0530

    practicing

---
+++ MyContactForm_PageObject/src/test/java/FrameWrok_EnvSetUp/Pathutility.java
@@ -0,0 +1,9 @@
+package FrameWrok_EnvSetUp;
+
+public class Pathutility {
+
+	public static final String DRIVER_PATH = "/Users/karthickshanmugam/Downloads/chromedriver";
+	public static final String IEDRIVER_PATH = null;
+
+
+}
